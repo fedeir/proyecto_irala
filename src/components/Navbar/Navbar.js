1f6e616bfcb741d914d35b230da6bd2cff34d19b
Federico Irala <fmirala@gmail.com>
2021-09-27 23:26:29 -0300

    incorporacion de navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css' 
+const navBar = () => {
+    return (
+        <div>
+           <nav> <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Nosotros</a></li>
+            </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default navBar
